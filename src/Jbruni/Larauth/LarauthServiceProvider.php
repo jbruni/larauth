167feb541c29659bb63aa98fa81f1baf056d9c6c
J. Bruni <contato@jbruni.com.br>
2013-08-30 17:15:27 -0300

    Initial commit

---
+++ src/Jbruni/Larauth/LarauthServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Jbruni\Larauth;
+
+use Illuminate\Support\ServiceProvider;
+
+class LarauthServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('jbruni/larauth');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
